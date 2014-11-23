00fde2ff47164bec4d2d9b150e2aa2da0bc58e67
Kenny Guo <dev_kg@163.com>
2014-11-23 13:59:47 +0800

    Add test for help page, and modify page to make tests passed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe "Static pages" do
+	# test home page
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
+		end
+	end
+	# test help page
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
 		end
 	end
 end
